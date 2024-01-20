ed8a5357df6c4f208b50fb546e62e020a2bb4f3a
oidb7 <omaridb77@gmail.com>
2024-01-20 12:16:39 -0800

    adding some files of this project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * I'm printing a string using the 'puts' function
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
