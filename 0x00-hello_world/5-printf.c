c418d0ea24633d9c01868801446ac5222f2d8cf2
oidb7 <omaridb77@gmail.com>
2024-01-24 08:41:06 -0800

    updates on 5-printf.c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,13 @@
 /**
  * main - is the entry point
  *
+ * I'm printing a string to the concole with the printf func
+ *
  * Return: 0 (success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
