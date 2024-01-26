208f202c3690e2c65eb379c05fa6a211e09a5ff7
oidb7 <omaridb77@gmail.com>
2024-01-26 10:24:39 -0800

    updat 101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	char s[59]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char s[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	fwrite(s, sizeof(s), 1, stderr);
 
