dbe554dd71f95f69f636f1c1e3c4228c2e5a4dab
oidb7 <omaridb77@gmail.com>
2024-01-26 10:11:33 -0800

    adding 101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the program
+ *
+ * this program is printing a quote to the standard error
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char s[59]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(s, sizeof(s), 1, stderr);
+
+	return (1);
+}
