296d38c869cbba52d8c07e495d56b6f608c56347
oidb7 <omaridb77@gmail.com>
2024-01-29 15:37:06 -0800

    ai is powerfull

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,25 +10,38 @@
 
 int main(void)
 {
-	int i, j;
-	for (i = 0; i < 100; i++)
+	int l1, l2, l3, l4, i;
+
+	l1 = '0';
+	while (l1 <= '9')
 	{
-		for (j = i; j < 100; j++)
+		l2 = '0';
+		while (l2 <= '9')
 		{
-			putchar(i / 10 + '0');
-			putchar(i % 10 + '0');
-			putchar(' ');
-			putchar(j / 10 + '0');
-			putchar(j % 10 + '0');
-			
-			if (i != 99 || j != 99)
+			l3 = l1, i = 1;
+			while (l3 <= '9')
 			{
-				putchar(',');
-				putchar(' ');
+				if (i)
+					l4 = l2 + 1, i = 0;
+				else
+					l4 = '0';
+				while (l4 <= '9')
+				{
+					putchar(l1);
+					putchar(l2);
+					putchar(' ');
+					putchar(l3);
+					putchar(l4);
+					if (l1 != '9' || l2 != '8' || l3 != '9' || l4 != '9')
+						putchar(','), putchar(' ');
+					l4++;
+				}
+				l3++;
 			}
+			l2++;
 		}
+		l1++;
 	}
 	putchar('\n');
-
-	return 0;
+	return (0);
 }
