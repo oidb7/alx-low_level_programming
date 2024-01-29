1f0a45014f1f7ae005af76ede2fae0151aa1b169
oidb7 <omaridb77@gmail.com>
2024-01-29 09:56:00 -0800

    print combs

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point
+ *
+ * Des: program that prints all possible combinations of single-digit numbers
+ *
+ * Retrn: 0 (success)
+ */
+
+int main(void)
+{
+	int dig = '0';
+
+	while (dig <= '9')
+	{
+		putchar(dig);
+
+		if (dig++ != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
