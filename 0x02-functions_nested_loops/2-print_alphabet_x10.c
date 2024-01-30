390d81cea95b5271bd0572e2fdfc3cbddcdcfb53
oidb7 <omaridb77@gmail.com>
2024-01-30 15:34:17 -0800

    up

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabe
+ *
+ * return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int index;
+
+	for (index = 0; index < 10; index++)
+	{
+		print_alphabet();
+	}
+}
