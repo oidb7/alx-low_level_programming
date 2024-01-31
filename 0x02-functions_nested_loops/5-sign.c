0eb27d699f6a7db85c5220720c89e6a921474025
oidb7 <omaridb77@gmail.com>
2024-01-31 01:32:41 -0800

    check sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of a digit
+ *
+ * @n: digit to check
+ *
+ * Return: 1 if positive, -1 if nigative, 0 if digit is zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+
+		return (1);
+	}
+
+	else if (n < 0)
+	{
+		_putchar('-');
+
+		return (0);
+	}
+
+	else
+	{
+		_putchar('0');
+
+		return (0);
+}
