e4bb225f90040434fc08839d8958e438825463c1
oidb7 <omaridb77@gmail.com>
2024-02-05 08:23:15 -0800

    add a to b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - calculates the addition of tow integers
+ *
+ * @a: first digit for calculation
+ *
+ * @b: second one
+ *
+ * Return: result of a + b
+ */
+
+int add(int a, int b)
+{
+	int result = (a + b);
+
+	return (result);
+}
