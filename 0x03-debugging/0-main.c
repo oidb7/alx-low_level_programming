e536e9259b966a32e11e78c5aaebce6b54b13674
oidb7 <omaridb77@gmail.com>
2024-05-02 05:58:47 -0700

    test until sertainty

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * main - tests the function that prints if the integer is positive or else.
- *
- * Return: 0 on success
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
 {
-	int n = 0;
-	positive_or_negative(n);
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
 
 	return (0);
 }
